6a7b10a9f5670e5483ae7c6699f8db67b7358078
Ahmed <ahmed@sobhis-air.home>
2019-06-26 18:41:58 +0300

    convert to MVC design pattern

---
+++ Todo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Ahmed on 6/25/19.
+//  Copyright © 2019 Ahmed sobhi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
